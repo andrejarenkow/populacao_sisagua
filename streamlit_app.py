830e4e18eee0a807febc42391af70496fd6976ec
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-03-06 11:48:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ import plotly.express as px
 # Hello SISAGUA
 
 st.write("Heloo, Sisagua")
+
+dados = pd.read_excel('https://drive.google.com/uc?export=download&id=1-3mDdJsT768n9gupjR3ZFDHdxlbY2td0')
+dados.head()
